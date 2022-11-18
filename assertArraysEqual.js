ee83edc0bb55131f6098e4e9445d2ea32da3b102
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2022-11-17 20:37:12 -0500

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,14 @@
+const eqArrays = function(array1, array2, expected) {
+  let isEq = true;
+  array1.length !== array2.length ? isEq = false : "";
+  for (let i = 0; i < array1.length; i++) {
+    array1[i] !== array2[i] ? isEq = false : "";
+  }
+  if (isEq === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${isEq} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${isEq} !== ${expected}`);
+  }
+};
+
+eqArrays([1, 2, 3, 4], [1, 2, 3, 4], false);
